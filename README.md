3b2fe2b3b2782c035f45dcebbeb7c793d6e6f0fc
Laurent Itti <jevois.org@gmail.com>
2021-09-01 14:17:00 -0700

    make sure we get required packages

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jevoispro-sdk
 Development and operating system tools for JeVois-Pro
+
+The JeVois-Pro SDK provides a U-Boot bootloader, Linux kernel, and full Ubuntu operating system image that can be
+flashed to microSD for use in the JeVois-Pro smart camera.
--- rebuild-os.sh
+++ rebuild-os.sh
@@ -10,7 +10,26 @@ cd ${r}/fenix
 
 sudo /bin/rm -rf build
 
-source env/setenv.sh -q -s  KHADAS_BOARD=JVPRO LINUX=4.9 UBOOT=2015.01 DISTRIBUTION=Ubuntu DISTRIB_RELEASE=focal DISTRIB_TYPE=xfce DISTRIB_ARCH=arm64 INSTALL_TYPE=SD-USB COMPRESS_IMAGE=no
+sudo apt install git dialog lsb-release binutils wget ca-certificates device-tree-compiler pv bc lzop zip \
+     binfmt-support build-essential ccache debootstrap ntpdate gawk gcc-arm-linux-gnueabihf gcc-8-arm-linux-gnueabihf \
+     qemu-user-static u-boot-tools uuid-dev zlib1g-dev unzip libusb-1.0-0-dev parted pkg-config libncurses5-dev \
+     whiptail debian-keyring debian-archive-keyring f2fs-tools libfile-fcntllock-perl rsync libssl-dev \
+     nfs-kernel-server btrfs-progs ncurses-term p7zip-full kmod dosfstools libc6-dev-armhf-cross fakeroot curl \
+     patchutils python2 python3 liblz4-tool libpython2.7-dev linux-base swig aptly acl systemd-container udev \
+     g++-10-arm-linux-gnueabihf lib32stdc++6 libc6-i386 lib32ncurses6 lib32tinfo6 locales ncurses-base zlib1g:i386 \
+     pixz bison libbison-dev flex libfl-dev lib32z1 tzdata cpio libglib2.0-dev libtool gettext bsdmainutils \
+     bash-completion
+
+source env/setenv.sh -q -s \
+       KHADAS_BOARD=JVPRO \
+       LINUX=4.9 \
+       UBOOT=2015.01 \
+       DISTRIBUTION=Ubuntu \
+       DISTRIB_RELEASE=focal \
+       DISTRIB_TYPE=xfce \
+       DISTRIB_ARCH=arm64 \
+       INSTALL_TYPE=SD-USB \
+       COMPRESS_IMAGE=no
 
 make clean # otherwise we get some weird error on first make, which disappears when trying make again...
 
